a2e26b1d2045ca7f9e5a523d6b3986beebc74317
ChieuTruong <115620071+ChieuTruong@users.noreply.github.com>
2023-01-16 20:00:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Công chúa à!',
+    introTitle: 'Công chúa à!!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
