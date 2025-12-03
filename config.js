ae28e894491b171d3c51363698dd531cbcb788d4
ChieuTruong <115620071+ChieuTruong@users.noreply.github.com>
2025-12-03 20:17:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const CONFIG = {
     btnIntro: 'Xác nhận :>',
     title: 'Vui lòng nhập số lượng muốn mua',
     desc: 'Nhập chính xác',
-    btnYes: 'Lẻ 1 bộ',
-    btnNo: '!000 bộ',
+    btnYes: '1000 bộ',
+    btnNo: 'Lẻ 1 bộ',
     question:'Anh chị đã chắc chắn chưa?',
     btnReply: 'Phản hồi',
     reply: 'Tôi xác nhận',
     mess: 'Em chốt đơn! ',
-    messDesc: 'okok',
+    messDesc: 'Tôi xin xác nhận',
     btnAccept: 'Xác nhận',
     messLink: 'https://www.youtube.com/watch?v=52i--_4joAM' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
